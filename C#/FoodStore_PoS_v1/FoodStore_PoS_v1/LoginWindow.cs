ea77550462fec0e0745a60da42022b3b10216a9d
Armannhj <armannhj@outlook.com>
2016-11-17 09:43:41 +0000

    Tími 1 done gerði c# byrjun og eitthvað í dagbók

---
+++ C#/FoodStore_PoS_v1/FoodStore_PoS_v1/LoginWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Ármann Hallbert Jónsson
+ * Gylfi Þór Helgason
+ * Hávar Sigurðarson
+ * HáG FoodStore
+ * Lokaverkefni haust önn 2016
+ */ 
+
+namespace FoodStore_PoS_v1
+{
+    public partial class LoginWindow : Form
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
